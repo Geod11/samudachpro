3edd561f221370b3207bb986c98d5bde2ca4747e
Geod11 <danilescugeorge@yahoo.com>
2024-06-06 16:52:54 +0300

    Added styled components to About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,14 +3,16 @@ import "../style/about.css";
 import ProjectVideo from "../Assets/video/1.mp4";
 // import { HashLink } from "react-router-hash-link";
 // import ContactForm from "./ContactForm";
+import styled from "styled-components";
 
 function About() {
   return (
-    <div id="topAbout">
-      <div className="about__mainImageContainer"></div>
-      <h2 className="about__main__title">DESPRE NOI</h2>
-      <div className="about__innerContainer">
-        <p className="about__main__description">
+    // <div id="headerAboutRoute">
+    <HeaderAboutRoute>
+      <MainImageContainer></MainImageContainer>
+      <MainTitle>DESPRE NOI</MainTitle>
+      <InnerContainer>
+        <MainDescription>
           Bine ați venit la Samudachpro, partenerul dumneavoastră de încredere
           în soluții de construcții. Cu o reputație solidă pentru excelență și
           angajamentul de a oferi rezultate excepționale, suntem mândri să fim
@@ -36,33 +38,102 @@ function About() {
           colabora și de a vă transforma visele de construcție în realitate.
           Contactați-ne astăzi pentru a discuta despre proiectul dumneavoastră
           și pentru a descoperi cum vă putem aduce viziunea la viață.
-        </p>
-        <hr className="about__hline_displayNone" />
-        <div className="about__featuredProjectContainer">
-          <h2 className="about__main__title">CALITATE</h2>
-          <video
-            playsinline="true"
-            webkit-playsinline="true"
-            muted
-            loop
-            src={ProjectVideo}
-            autoplay="true"
-            className="about__video"
-          ></video>
-          {/* <HashLink smooth to="/projects#topProjects" className="about__CTA">
+        </MainDescription>
+        {/* <hr className="about__hline_displayNone" /> */}
+        {/* <div className="about__featuredProjectContainer"> */}
+        {/* <h2 className="about__main__title">CALITATE</h2> */}
+        <Video
+          playsinline="true"
+          webkit-playsinline="true"
+          muted
+          loop
+          src={ProjectVideo}
+          autoplay="true"
+          className="about__video"
+        ></Video>
+        {/* <HashLink smooth to="/projects#topProjects" className="about__CTA">
             <button className="about__btn btn btn-primary">
               Afla mai mult
             </button>
           </HashLink> */}
-        </div>
-      </div>
-      <hr className="about__hline_displayBig" />
+        {/* </div> */}
+      </InnerContainer>
+      {/* <hr className="about__hline_displayBig" /> */}
       {/* Why us <p> */}
 
       {/* Contact form */}
       {/* <ContactForm className="form-in-aboutUs" hideLogo centerForm /> */}
-    </div>
+      {/* </div> */}
+    </HeaderAboutRoute>
   );
 }
 
 export default About;
+
+const HeaderAboutRoute = styled.div`
+  /* @media (min-width: 768px) {
+    &div.about__whyUs__container&h2 {
+      width: 100%;
+    }
+    &div.about__innerContainer&p {
+      width: 35%;
+      padding: 0;
+    }
+  } */
+  @media (min-width: 992px) {
+    padding: 0 5% 35px 5%;
+  }
+  @media (min-width: 1400px) {
+    padding: 25px 5% 60px 5%;
+  }
+`;
+
+const MainImageContainer = styled.div`
+  height: 100px;
+  color: white;
+  text-align: center;
+  background-image: url("../Assets/Background-pictures/josh-olalde-X1P1_EDNnok-unsplash.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  /* @media (min-width: 768px) {
+    height: 150px;
+  } */
+  @media (min-width: 992px) {
+    height: 170px;
+  }
+`;
+
+const MainTitle = styled.h2`
+  text-align: center;
+  margin: 30px 0 40px 0;
+  @media (min-width: 1400px) {
+    margin: 0px 0 40px 0;
+  }
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  position: relative;
+  @media (min-width: 992px) {
+    padding: 0 5%;
+  }
+`;
+
+const MainDescription = styled.p`
+  font-size: large;
+  padding: 0 2rem 30px 2rem;
+  @media (min-width: 992px) {
+    font-size: larger;
+  }
+  @media (min-width: 1400px) {
+    font-size: x-large;
+  }
+`;
+
+const Video = styled.video`
+  width: 300px;
+  margin-right: 20px;
+`;
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -40,37 +40,6 @@ function Contact() {
       <div className="contact__container__instructionsPicture">
         <ContactForm />
         <ContactDetailsCard />
-        {/* <div className="contact__instructions__container">
-          <div>
-            <h2 className="contact__instructions__title">Contact</h2>
-            <p className="contact__instructions__info">
-              <span>Adresă</span>
-              <br />
-              Strada Lungă nr.19
-              <br />
-              Orașul Ocna Mureș
-              <br />
-              Județul Alba
-              <br />
-              <br />
-              <span>Program</span>
-              <br />
-              Luni - Vineri : 08:00-17:00
-              <br />
-              Sâmbătă - Duminică : Închis
-              <br />
-              <br />
-              <span>Telefon</span>
-              <br />
-              +40&nbsp;753&nbsp;769943
-              <br />
-              <br />
-              <span>Email</span>
-              <br />
-              samuioan12@icloud.com{" "}
-            </p>
-          </div>
-        </div> */}
       </div>
     </div>
   );
